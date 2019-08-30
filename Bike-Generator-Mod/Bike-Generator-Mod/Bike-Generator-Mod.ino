c859f1b17abab98de7c5b4d56d9a9483df8724e0
Joe Meyer <jmeyer@smm.org>
2019-08-30 10:55:50 -0500

    rename var

--- Bike-Generator-Mod/Bike-Generator-Mod/Bike-Generator-Mod.ino
+++ Bike-Generator-Mod/Bike-Generator-Mod/Bike-Generator-Mod.ino
@@ -1,10 +1,11 @@
-const int analogPin = A0;   // the pin that the potentiometer is attached to
-const int lampCount = 6;    // the number of lights being used on each side
-const int toggleSwitch = 15; // the pin to toggleSwitch between incandesent and LED lights.
+#define generatorPin A0  // the generator voltage divider pin
+#define switchPin 15     // the switch that toggles between incandescent and LED 
 
-bool inLedLampMode = false; //toggle to track if LED or Incandescent
 int IncPins[] = {2, 3, 4, 5, 6, 7};   // an array of pin numbers to which incandescent bulb drivers are attached
 int LedPins[] = {8, 9, 10, 11, 12, 13};  // an array of pin numbers to which LED bulb drivers are attached
+
+const int lampCount = 6;    // the number of lights being used on each side
+bool inLedLampMode = false; //toggle to track if LED or Incandescent
 int states[6];  // the states of the 6 lights. Lit(1) or not(0).
 int clearStates[] = {1, 1, 1, 1, 1, 1};  // Array used to turn off lights on side not lit.
 int thresholds[] = {250, 280, 310, 340, 360, 380}; // thresholds for each light.
@@ -12,7 +13,7 @@ int hyst = 50; //hysteresis so lights turn off at a lower level they turn on.
 
 void setup() {
   Serial.begin(9600);
-  pinMode(15, INPUT_PULLUP); //toggleSwitch - incandescents or LEDs
+  pinMode(15, INPUT_PULLUP); //switchPin - incandescents or LEDs
   for (int i = 0; i < lampCount; i++) { // setup incandesence
     pinMode(IncPins[i], OUTPUT);
     digitalWrite(IncPins[i], HIGH);
@@ -26,7 +27,7 @@ void setup() {
 }
 
 void loop() {
-  int sensorReading = analogRead(analogPin);  //read voltage of generator
+  int sensorReading = analogRead(generatorPin);  //read voltage of generator
 
   //Serial.println(sensorReading);
   
@@ -38,14 +39,14 @@ void loop() {
     }
   }
 
-  if (digitalRead(toggleSwitch)) {  //switched to LED lamp mode, switch is "HIGH"
+  if (digitalRead(switchPin)) {  //switched to LED lamp mode, switch is "HIGH"
     if (!inLedLampMode) {
       lights(IncPins, clearStates); //turn off all incandesent lamps
       inLedLampMode = true;         //set toggle so it doesn't clear each loop
     }
     lights(LedPins, states);        //turn on the led lamps according to states
   }
-  else if (!digitalRead(toggleSwitch)) { //switched to incandescent lamp mode, switch is "LOW"
+  else if (!digitalRead(switchPin)) { //switched to incandescent lamp mode, switch is "LOW"
     if (inLedLampMode) {
       lights(LedPins, clearStates); //turn off all led lamps
       inLedLampMode = false;
